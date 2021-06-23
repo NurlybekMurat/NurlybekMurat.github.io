f72c5207dae6e4819a6f0e3af3b45c5d8c5397d6
nurlybekmurat <48953078+nurlybekmurat@users.noreply.github.com>
2021-06-23 10:56:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const navBtn = document.querySelector("#nav-btn");
+const overlay = document.querySelector("#menu");
+const closeBtn = document.querySelector("#closeBtn");
+
+navBtn.addEventListener("click", function () {
+  navBtn.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+closeBtn.addEventListener("click", () => {
+  navBtn.classList.remove("close");
+  overlay.classList.remove("overlay");
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640 : {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 90,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".cardsSlider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".magazineSlider", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    640 : {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 90,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
